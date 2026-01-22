70b7a5356f9f1841ee1604ca58ec9a1e9c0e52c2
shilpayadawad-321 <shilpayadawad3@gmail.com>
2026-01-22 00:57:41 -0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+students=["Somanth","Raghav","Ravi"] 
+print(students)
+print(students[0])
+print(students[-1])
+print(students[-3:-1])
+students.append("Rakshita")
+students.insert(1,"Shankar")
+students.remove("Rakshita")
+print(students)
+students.sort()
+print(students)
+students.reverse()
+print(students)
+len(students)
+['Somanth', 'Raghav', 'Ravi']
+Somanth
+Ravi
+['Somanth', 'Raghav']
+['Somanth', 'Shankar', 'Raghav', 'Ravi']
+['Raghav', 'Ravi', 'Shankar', 'Somanth']
+['Somanth', 'Shankar', 'Ravi', 'Raghav']
+
